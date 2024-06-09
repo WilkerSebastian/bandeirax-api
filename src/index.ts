3d07b7fcc7d881a34676acb4ec2b379d067d6bf7
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-06-09 18:05:42 -0400

    add: database connection test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./router";
+import db from "./database/connection";
 
 const app = express()
 
@@ -10,4 +11,18 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(router)
 
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+app.listen(PORT, async () => {
+ 
+    try {
+    
+        await db.connect()
+
+        console.log(`Running on port ${PORT}`)
+
+    } catch (error) {
+        
+        console.log(error)
+
+    }
+
+})
