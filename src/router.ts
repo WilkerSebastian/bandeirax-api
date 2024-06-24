5d1e1f8abbf73fe46637d91d549abc1fb213e1eb
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-06-24 17:08:58 -0400

    up: complete new routes of new methods of user service

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,18 @@ const router = Router();
 
 router.get("/", HelperController.index)
 
-router.post("/user/create/", UserController.create)
+router.post(`/user/create/`, UserController.create)
 
-router.put("/user/validate/:id", UserController.validateView)
+router.patch("/user/validate/:id", UserController.validateView)
 
 router.get("/validate/:message", UserController.validateView)
+
+router.get("/user/verified/login/", UserController.verifiedLogin)
+
+router.put("/user/update/", UserController.update)
+
+router.delete("/user/delete/:key/:id", UserController.delete)
+
+router.get("/user/count/", UserController.count)
 
 export default router;
