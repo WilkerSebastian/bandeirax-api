406d26f423869acd5afe107e7ab80c0c04e9fcc6
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-06-24 23:11:33 -0400

    up: connection for production

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,29 @@
 import { Pool } from "pg"
 
-const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: 5432
-})
+let db: Pool
+
+if (process.env.NODE_ENV == "development") {
+
+    db = new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: 5432
+    })
+
+} else {
+
+    db = new Pool({
+        connectionString: process.env.URL,
+        ssl:{
+            rejectUnauthorized:false,
+            requestCert: true
+        }
+    })
+
+    
+}
 
 export const query = async (text: string, params?: any[]) => {
     const client = await db.connect();
